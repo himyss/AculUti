994d757ecdf00459db911364e6dedd2d11363ac6
Muzalevsky Ivan <muzalevsky@exolina3.jinr.ru>
2018-05-11 14:20:07 +0300

    modified

--- macroexp18/calib.C
+++ macroexp18/calib.C
@@ -5,19 +5,19 @@ using namespace std;
 
 void calib()
 {
-	gSystem->Load("/home/muzalevsky/AculUti/libAculData.so");
-	gSystem->Load("/home/muzalevsky/AculUti/libTELoss.so");
+	gSystem->Load("/home/muzalevsky/soft/AculUti/libAculData.so");
+	gSystem->Load("/home/muzalevsky/soft/AculUti/libTELoss.so");
 
 	AculCalibration cal;
-	cal.SetWorkDirectory("/home/muzalevsky/AculUti/exp1803/cali1mm/calDL/");
-	cal.SetParFileName("/home/muzalevsky/AculUti/macroexp18/parforcal.par");	
-	cal.SetInputRootFile("/media/users_NAS/Muzalevsky/si_1000_LR_02_0001.root");
+	cal.SetWorkDirectory("/home/muzalevsky/work/exp1803/data/dataCal/");
+	cal.SetParFileName("/home/muzalevsky/soft/AculUti/macroexp18/parforcal.par");	
+	cal.SetInputRootFile("/media/analysis_nas/exp201804/calib/si_1000_LR_02_0001.root");
 	cal.Init();	//takes parameters from .par
 	cal.PrintInputParameters();
 
 
 	cal.FindPedestals(0, 50);
-	cal.Mycalc(150, 400);
+	cal.Mycalc(150, 500);
 	cal.FindEnergyPedestals();
 	cal.ShowFullCalibratedSpectra(0, 4095);
 
